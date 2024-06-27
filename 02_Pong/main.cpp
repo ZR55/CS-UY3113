3f20d97dd47781a4fa8288c175c3f3b23210fd34
Rui Zhang <zhangduanrui57@gmail.com>
2024-06-27 16:08:59 -0400

    made the ball bounce

--- 02_Pong/main.cpp
+++ 02_Pong/main.cpp
@@ -24,6 +24,7 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "ShaderProgram.h"
 #include "stb_image.h"
+#include "glm/ext.hpp"
 
 /* enums */
 enum AppStatus {RUNNING, TERMINATED};
@@ -32,6 +33,9 @@ enum AppStatus {RUNNING, TERMINATED};
 // The size of our literal game window
 constexpr int WINDOW_WIDTH = 640,
 WINDOW_HEIGHT = 480;
+
+constexpr float WIDTH_BOUND = 4.9f,
+HEIGHT_BOUND = 3.6f;
 
 // general
 constexpr float BG_RED = 193 / 255.0f,
@@ -51,20 +55,20 @@ constexpr char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
 F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
 
 constexpr char PADDLE_SPRITE_FILEPATH[] = "assets/paddle.png";
-constexpr float PADDLE_WIDTH = 1.0f;
-constexpr float PADDLE_HEIGHT = 630 / 498.0f;
+constexpr float PADDLE_WIDTH = 0.3f;
+constexpr float PADDLE_HEIGHT = 630 / 108.0f * PADDLE_WIDTH;
 constexpr glm::vec3 PADDLE_INIT_SCALE = glm::vec3(PADDLE_WIDTH, PADDLE_HEIGHT, 0.0f);
-constexpr glm::vec3 PADDLE_LEFT_INIT_POS = glm::vec3(-4.9f, 0.0f, 0.0f);
-constexpr glm::vec3 PADDLE_RIGHT_INIT_POS = glm::vec3(4.9f, 0.0f, 0.0f);
+constexpr glm::vec3 PADDLE_LEFT_INIT_POS = glm::vec3(-WIDTH_BOUND, 0.0f, 0.0f);
+constexpr glm::vec3 PADDLE_RIGHT_INIT_POS = glm::vec3(WIDTH_BOUND, 0.0f, 0.0f);
 constexpr char BALL_SPRITE_FILEPATH[] = "assets/ball.png";
 constexpr float BALL_SIZE = 0.3f;
 constexpr glm::vec3 BALL_INIT_SCALE = glm::vec3(BALL_SIZE, BALL_SIZE, 0.0f);
+//constexpr float BALL_INIT_SPEED_X = 2.0f;
+//constexpr float BALL_INIT_SPEED_Y = 1.7f;
 
 constexpr int NUMBER_OF_TEXTURES = 1;
 constexpr GLint LEVEL_OF_DETAIL = 0;
 constexpr GLint TEXTURE_BORDER = 0;
-
-//constexpr float ROT_INCREMENT_WATERMELON = 1.0f;
 
 /* global variables */
 // general
@@ -80,15 +84,8 @@ g_paddle_right_texture_id,
 g_ball_texture_id;
 
 // objects
-//float g_theta_watermelon = 0.0f,
-//g_theta_bin_apple = 10.0f,
-//g_theta_apple = 0.0f;
-
-//float g_paddle_left_x = -4.9f,
-//g_paddle_right_x = 4.9f,
-//g_paddle_y = 0.0f;
-
-float g_paddle_speed = 1.0f;
+float g_paddle_speed = 2.0f;
+float g_ball_speed = 1.0f;
 
 glm::mat4 g_view_matrix,
 g_paddle_left_matrix,
@@ -98,8 +95,10 @@ g_projection_matrix;
 
 glm::vec3 g_paddle_left_position = glm::vec3(0.0f);
 glm::vec3 g_paddle_right_position = glm::vec3(0.0f);
+glm::vec3 g_ball_position = glm::vec3(0.0f);
 glm::vec3 g_paddle_left_movement = glm::vec3(0.0f);
 glm::vec3 g_paddle_right_movement = glm::vec3(0.0f);
+glm::vec3 g_ball_movement = glm::vec3(2.0f, 1.7f, 0.0f);
 
 
 GLuint load_texture(const char* filepath) {
@@ -190,40 +189,6 @@ void process_input() {
             g_game_status = TERMINATED;
         }
     }
-    //switch (event.type) {
-    //    case SDL_QUIT:
-    //    case SDL_WINDOWEVENT_CLOSE:
-    //        g_game_status = TERMINATED;
-    //        break;
-
-    //    case SDL_KEYDOWN:
-    //        switch (event.key.keysym.sym) {
-    //            case SDLK_UP:
-    //                g_paddle_right_movement.y = -1.0f;
-    //                break;
-
-    //            case SDLK_DOWN:
-    //                g_paddle_right_movement.y = 1.0f;
-    //                break;
-
-    //            case SDLK_w:
-    //                g_paddle_left_movement.y = -1.0f;
-    //                break;
-
-    //            case SDLK_s:
-    //                g_paddle_left_movement.y = 1.0f;
-    //                break;
-
-    //            case SDLK_t:
-    //                break;
-
-    //            default:
-    //                break;
-    //        }
-
-    //    default:
-    //        break;
-    //}
 
     const Uint8* key_state = SDL_GetKeyboardState(NULL);
 
@@ -262,12 +227,52 @@ void update() {
     g_previous_tick = tick;
 
     // game logic - accumulators
-    //g_theta_watermelon += 1.5f * delta_time;
-    //g_rotation_watermelon.z += ROT_INCREMENT_WATERMELON * delta_time;
-    //g_theta_bin_apple += 1.7f * delta_time;
-    //g_theta_apple += 4.5f * delta_time;
     g_paddle_left_position += g_paddle_left_movement * g_paddle_speed * delta_time;
     g_paddle_right_position += g_paddle_right_movement * g_paddle_speed * delta_time;
+    g_ball_position += g_ball_movement * g_ball_speed * delta_time;
+    //std::cout << glm::to_string(g_paddle_right_position) << std::endl;
+
+    // collision detection
+    // up and bottom
+    if (g_ball_position.y >= HEIGHT_BOUND) {
+        // put it back to the max position to avoid hanging
+        g_ball_position.y = HEIGHT_BOUND - BALL_SIZE / 2;
+        g_ball_movement.y *= -1;
+    }
+    else if (g_ball_position.y <= -HEIGHT_BOUND) {
+        g_ball_position.y = -HEIGHT_BOUND + BALL_SIZE / 2;
+        g_ball_movement.y *= -1;
+    }
+    // with the right paddle
+    if (g_ball_position.x >= PADDLE_RIGHT_INIT_POS.x - PADDLE_WIDTH / 2 &&
+        g_ball_position.y <= g_paddle_right_position.y + PADDLE_HEIGHT / 2 &&
+        g_ball_position.y >= g_paddle_right_position.y - PADDLE_HEIGHT / 2) {
+    //if (g_ball_position.x <= PADDLE_RIGHT_INIT_POS.x - PADDLE_WIDTH / 2) {
+
+        // put the ball back on the paddle   
+        //g_ball_position.x = PADDLE_RIGHT_INIT_POS.x - PADDLE_WIDTH / 2 - BALL_SIZE / 2;
+        g_ball_movement.x *= -1;
+        LOG("BOUNCE RIGHT!!\n\n");
+    }
+    if (g_ball_position.x <= PADDLE_LEFT_INIT_POS.x + PADDLE_WIDTH / 2 &&
+        g_ball_position.y <= g_paddle_left_position.y + PADDLE_HEIGHT / 2 &&
+        g_ball_position.y >= g_paddle_left_position.y - PADDLE_HEIGHT / 2) {
+        g_ball_position.x = PADDLE_LEFT_INIT_POS.x + PADDLE_WIDTH / 2 + BALL_SIZE / 2;
+        g_ball_movement.x *= -1;
+        LOG("BOUNCE LEFT!!\n\n");
+
+    }
+
+    //float x_distance = fabs(g_flower_position.x - CUP_INIT_POS.x) - ((FLOWER_INIT_SCA.x + CUP_INIT_SCA.x) / 2.0f);
+    //float y_distance = fabs(g_flower_position.y - CUP_INIT_POS.y) - ((FLOWER_INIT_SCA.y + CUP_INIT_SCA.y) / 2.0f);
+    //std::cout << "x = " << g_ball_position.x << "\n";
+    //std::cout << "paddle right BOUND " << PADDLE_RIGHT_INIT_POS.x - PADDLE_WIDTH / 2 << "\n";
+    //std::cout << "paddle left BOUND " << PADDLE_LEFT_INIT_POS.x + PADDLE_WIDTH / 2 << "\n";
+    std::cout << "y = " << g_ball_position.y << "\n";
+    std::cout << "paddle right top bound =  " << g_paddle_right_position.y + PADDLE_HEIGHT / 2 << "\n";
+    std::cout << "paddle right bottom bound =  " << g_paddle_right_position.y - PADDLE_HEIGHT / 2 << "\n";
+
+
 
     // model matrix reset
     g_paddle_left_matrix = glm::mat4(1.0f);
@@ -275,80 +280,14 @@ void update() {
     g_ball_matrix = glm::mat4(1.0f);
 
     // transformation
+    g_ball_matrix = glm::translate(g_ball_matrix, g_ball_position);
+    g_ball_matrix = glm::scale(g_ball_matrix, BALL_INIT_SCALE);
     g_paddle_left_matrix = glm::translate(g_paddle_left_matrix, PADDLE_LEFT_INIT_POS);
     g_paddle_left_matrix = glm::translate(g_paddle_left_matrix, g_paddle_left_position);
+    g_paddle_left_matrix = glm::scale(g_paddle_left_matrix, PADDLE_INIT_SCALE);
     g_paddle_right_matrix = glm::translate(g_paddle_right_matrix, PADDLE_RIGHT_INIT_POS);
     g_paddle_right_matrix = glm::translate(g_paddle_right_matrix, g_paddle_right_position);
-    g_ball_matrix = glm::scale(g_ball_matrix, BALL_INIT_SCALE);
-    //if (g_watermelon_status == ESCAPING) {
-    //    // watermelon move in circle and rotate
-    //    glm::vec3 watermelon_translation_vector = glm::vec3(glm::cos(g_theta_watermelon) * 2.5, glm::sin(g_theta_watermelon) * 2.5, 0.0f);
-    //    g_paddle_right_matrix = glm::translate(g_paddle_right_matrix, watermelon_translation_vector);
-    //    g_paddle_right_matrix = glm::rotate(g_paddle_right_matrix, g_rotation_watermelon.z, glm::vec3(0.0f, 0.0f, -1.0f));
-    //    // bin moves in circle, chasing watermelon with slightly faster speed
-    //    glm::vec3 bin_translation_vector = glm::vec3(glm::cos(g_theta_bin_apple) * 2.5, glm::sin(g_theta_bin_apple) * 2.5, 0.0f);
-    //    g_ball_matrix = glm::translate(g_ball_matrix, bin_translation_vector);
-    //    g_ball_matrix = glm::scale(g_ball_matrix, BIN_INIT_SCALE);
-    //    // apple moves up and down inside of the bin
-    //    glm::vec3 apple_translation_vector = bin_translation_vector + glm::vec3(0.0f, glm::sin(g_theta_apple) * 0.3, 0.0f);
-    //    //g_apple_matrix = glm::translate(g_apple_matrix, bin_translation_vector);
-    //    g_paddle_left_matrix = glm::translate(g_paddle_left_matrix, apple_translation_vector);
-
-    //    // check if watermelon is caught
-    //    if (watermelon_translation_vector.x + FRUIT_WIDTH / 2 <= bin_translation_vector.x + BIN_SIZE / 2 - 0.5f &&
-    //        watermelon_translation_vector.x - FRUIT_WIDTH / 2 >= bin_translation_vector.x - BIN_SIZE / 2 + 0.5f &&
-    //        watermelon_translation_vector.y + WATERMELON_HEIGHT / 2 <= bin_translation_vector.y + BIN_SIZE / 2 - 0.2f &&
-    //        watermelon_translation_vector.y - WATERMELON_HEIGHT / 2 >= bin_translation_vector.y - BIN_SIZE / 2 + 0.2f) {
-    //        g_watermelon_status = CAUGHT;
-
-    //        std::cout << "CAUGHT!!!!!!" << '\n';
-
-    //        g_bin_x = bin_translation_vector.x;
-    //        g_bin_y = bin_translation_vector.y;
-    //        g_apple_x = apple_translation_vector.x;
-    //        g_apple_y = apple_translation_vector.y;
-    //        g_watermelon_x = watermelon_translation_vector.x;
-    //        g_watermelon_y = watermelon_translation_vector.y;
-    //    }
-    //}
-    //else {
-    //    // the bin with watermelon and apple move to the center
-    //    float back_to_origin_time = 1000.f;
-    //    float bin_increment_x = (0 - g_bin_x) / back_to_origin_time,
-    //        bin_increment_y = (0 - g_bin_y) / back_to_origin_time,
-    //        apple_increment_x = (0 - g_apple_x) / back_to_origin_time,
-    //        apple_increment_y = (0 - g_apple_y) / back_to_origin_time,
-    //        watermelon_increment_x = (0 - g_watermelon_x) / back_to_origin_time,
-    //        watermelon_increment_y = (0 - g_watermelon_y) / back_to_origin_time;
-    //    g_bin_x += bin_increment_x;
-    //    g_bin_y += bin_increment_y;
-    //    g_apple_x += apple_increment_x;
-    //    g_apple_y += apple_increment_y;
-    //    g_watermelon_x += watermelon_increment_x;
-    //    g_watermelon_y += watermelon_increment_y;
-    //    g_bin_size += 0.0005f;
-
-    //    if (g_bin_size >= 6.f) {
-    //        g_bin_size = 6.f;
-    //    }
-
-    //    g_ball_matrix = glm::translate(g_ball_matrix, glm::vec3(g_bin_x, g_bin_y, 0.f));
-
-    //    // the bin scales up once it gets to the origin
-    //    if (g_bin_x <= 0.05f && g_bin_y <= 0.05f) {
-    //        g_ball_matrix = glm::scale(g_ball_matrix, glm::vec3(g_bin_size, g_bin_size, 0.f));
-    //    }
-    //    else {
-    //        g_ball_matrix = glm::scale(g_ball_matrix, BIN_INIT_SCALE);
-    //    }
-
-    //    g_paddle_right_matrix = glm::translate(g_paddle_right_matrix, glm::vec3(g_watermelon_x, g_watermelon_y, 0.f));
-
-    //    g_paddle_left_matrix = glm::translate(g_paddle_left_matrix, glm::vec3(g_apple_x, g_apple_y, 0.f));
-    //}
-
-
-
+    g_paddle_right_matrix = glm::scale(g_paddle_right_matrix, PADDLE_INIT_SCALE);
 }
 
 void draw_object(glm::mat4& object_g_model_matrix, GLuint& object_texture_id) {
